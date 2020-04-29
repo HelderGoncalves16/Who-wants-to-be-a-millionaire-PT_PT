485b8d1fb08f0badff8919a6733ec555357a0969
Hélder Gonçalves <46654418+HelderGoncalves16@users.noreply.github.com>
2020-04-29 23:37:34 +0100

    Login and Register Done
    Login and Register Done. Working now on showing up the questions. Extracting them from txt file

--- ProgI_22067/main.c
+++ ProgI_22067/main.c
@@ -3,34 +3,48 @@
 #include<string.h>
 #include<locale.h>
 
-static int i=0;
-
-struct utilizador
-
-{
-
-char name[30],pass[30];
-
-}w[99];
-
-int n;
+int x, i, n, questaoNumero, tentativa, recebeQuestao=0, totalGanho;
+int premios[15]={25,50,125,250,500,750,1500,2500,5000,10000,16000,32000,64000,125000,250000};
+char nome[100], vidas[3];
 
 void login(void);
-
 void reg(void);
+void mostraPerguntas(int nivel);
+	
+struct utilizador{
+	char name[30];
+	char pass[30];
+	int tentativas;
+	char score[7];
+}w[99];
 
-
-	int main(){
+struct niveis{
+	char pergunta[120];
+	char opcA[70];
+	char opcB[70];
+	char opcC[70];
+	char opcD[70];
+	char resposta[5]; 	
+}perguntas[15]; 		// Esta Struct Recebe os ficheiros que contem as perguntas
+						// Cada ficheiro contém 15 perguntas
+	
+int main(){
 	
 	setlocale(LC_ALL, "Portuguese");
-
+	
+	//Reset
+	totalGanho = 0;
+	questaoNumero = 0;
+	vidas[0] = 'X';
+	vidas[1] = 'X';
+	vidas[2] = 'X';
+	
 	printf("\n\t\t\t\t\t\tBEM VINDO AO");
 	printf("\n\t\t\t\t\tQUEM QUER SER MILIONÁRIO");
 	printf("\n\n\t\t\t\t=============================================");
 	printf("\n\t\t\t\t\tPressiona Enter para Continuar!! ");
 	
 	if(getch()==13) // Tecla Enter
-
   
 		XY:
 	
@@ -89,21 +103,29 @@ void reg()
 
     FILE *fp;
 
-    char c,checker[30]; int z=0;
+    char c; 
+	int z=0;
 
     fp=fopen("Utilizadores.txt","ab+");
 
-    printf("\n\n\t\t\t\ZONA DE REGISTO");
+    printf("\n\n\t\t\t\t\tZONA DE REGISTO");
 
-    printf("\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^^^");
+    printf("\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 
     for(i=0;i<100;i++)
 
     {
+    	do {
+    		
+      		printf("\n\n\t\t\t\tDIGITE O SEU USERNAME: ");
 
-      printf("\n\n\t\t\t\tDIGITE O SEU USERNAME: ");
+      		scanf("%s",nome);
+      
+      		if(strlen(nome)<4 || strlen(nome)>12){
+      		printf("\n\t\tO seu Username deve conter mais de 4 caracteres e menos de 12");
+	  		}
 
-      scanf("%s",checker);
+}while(strlen(nome)<4 || strlen(nome)>12);
 
         while(!feof(fp))
 
@@ -111,7 +133,7 @@ void reg()
 
           fread(&w[i],sizeof(w[i]),1,fp);
 
-          if(strcmp(checker,w[i].name)==0) //Verifica se o username já existe no ficheiro
+          if(strcmp(nome,w[i].name)==0) //Verifica se o username já existe no ficheiro
 
             {
 
@@ -126,7 +148,7 @@ void reg()
 
           {
 
-            strcpy(w[i].name,checker);
+            strcpy(w[i].name,nome);
 
             break;
 
@@ -159,9 +181,9 @@ void reg()
 
         printf("\n\n\t\tRegisto Efetuado com Sucesso!");
 
-        printf("\n\n\t\tTry login your account??\n\n\t\t  ");
+        printf("\n\n\t\tEFETUAR LOGIN NA SUA CONTA?\n\n\t\t  ");
 
-        printf("> PRESS 1 FOR YES\n\n\t\t  > PRESS 2 FOR NO\n\n\t\t\t\t");
+        printf(">1. SIM\n\n\t\t  >2. NÃO\n\n\t\t\t\t");
 
         scanf("%d",&n);
 
@@ -172,19 +194,17 @@ void reg()
               case 1: 
 
                     login();
-
                     break;
 
               case 2: 
 
                     printf("\n\n\n\t\t\t\t\tOBRIGADO POR SE REGISTAR!");
-
                     break;
 
           }
 
         }
-
+        
         break;
 
       }
@@ -201,7 +221,7 @@ void reg()
 
       FILE *fp;
 
-      char c,name[30],pass[30]; int z=0;
+      char c,pass[30]; int z=0;
 
       int checku,checkp;
 
@@ -217,7 +237,7 @@ void reg()
 
         printf("\n\n\t\t\t\tUSERNAME: ");
 
-        scanf("%s",name);
+        scanf("%s",nome);
 
         printf("\n\n\t\t\t\tPASSWORD: ");
 
@@ -239,7 +259,7 @@ void reg()
 
         fread(&w[i],sizeof(w[i]),1,fp);
 
-          checku=strcmp(name,w[i].name);
+          checku=strcmp(nome,w[i].name);
 
           checkp=strcmp(pass,w[i].pass);
 
@@ -247,20 +267,18 @@ void reg()
 
           {
 
-            
 			system("cls");
-            printf("\n\n\n\t\t\tLOGIN EFETUADO COM SUCESSO!!");
-            printf("\n\n\n\t\t\t\tWELCOME, HAVE A NICE DAY");
-
+            printf("\n\n\n\t\t\tLOGIN EFETUADO COM SUCESSO!!\n");
+			
+			primeiroPatamar();
             break;
-
           }
 
         else if(checku==0&&checkp!=0)
 
           {
 
-            printf("\n\n\n\t\t\tPassword Errada!! Not %s??",name);
+            printf("\n\n\n\t\t\tPassword Errada!! Not %s??",nome);
 
             printf("\n\n\t\t\t\t  (Pressiona 'Y' para tentar novamente)");
 
@@ -279,6 +297,8 @@ void reg()
 
             if(getch()==13) {
             	reg();
+			} else {
+				main();
 			} 
           } 
         }
@@ -290,6 +310,112 @@ void reg()
       getch();
 
     }
+    
+    int primeiroPatamar(){
+    	
+    	FILE *primeiroNivel;
+    	
+    	printf("\n\t\t\tBEM-VINDO À PRIMEIRA FASE DO QUEM QUER SER MILIONÁRIO");
+    	printf("\n\t\t\tNESTA FASE SERÃO 5 PERGUNTAS\n");
+    	printf("\t\t\tCUJO VALORES PARA CADA PERGUNTA É DE: \n\t\t\t1 - 25 Euros\n\t\t\t2 - 50 Euros\n\t\t\t3 - 125 Euros\n\t\t\t4 - 250 Euros\n\t\t\t5 - 500 Euros\n");
+		printf("\t\t\tPREPARADO %s ? VAMOS COMEÇAR?", nome);
+		
+		if(getch()==13){
+			
+			if((primeiroNivel = fopen("primeiroNivel.txt","r"))==NULL){
+				erroFicheiro();
+			} else {
+				while(!feof(primeiroNivel)){
+					for(x=0; x<30; x++){
+						fgets(perguntas[x].pergunta, 120, primeiroNivel);
+						fgets(perguntas[x].opcA, 70, primeiroNivel);
+						fgets(perguntas[x].opcB, 70, primeiroNivel);
+						fgets(perguntas[x].opcC, 70, primeiroNivel);
+						fgets(perguntas[x].opcD, 70, primeiroNivel);
+						fgets(perguntas[x].resposta, 70, primeiroNivel);
+					}
+				}
+			}
+		}
+		
+		mostraPerguntas(1); //Mostrar as Perguntas Armazenadas
+		fclose(primeiroNivel);
+	}
+	
+	int segundoPatamar(){
+		return 0;
+	}
+	
+	void mostraPerguntas(int nivel){
+		
+		char opc;
+		int contadorQuestao; // Numero Total de Questões por nível
+		int questaoMostra[5]={-1,-1,-1,-1,-1};
+		int qu=0; //Para enviar questoes para o questaoMostra;
+		
+		
+		system("cls");
+		srand(time(NULL));
+		
+		for(contadorQuestao=0; contadorQuestao<5; contadorQuestao++){
+			
+			questaoNumero++;
+			
+			do{
+				
+				recebeQuestao=(rand()%15); //Gera um numero até Random até 15
+			
+			}while(recebeQuestao == questaoMostra[0] || recebeQuestao == questaoMostra[1] || recebeQuestao == questaoMostra[2] || recebeQuestao == questaoMostra[3] || recebeQuestao == questaoMostra[4]);
+			
+			questaoMostra[qu]=recebeQuestao;
+			
+			if(questaoNumero!=1){ //Este codigo nao pode aparecer logo na primeira Questão (Possiblidade de Desistir)
+				
+				printf("\n\t\t\tPretende Continuar %s ?", nome);
+				printf("\n\t\t\tA - Continuar\n\t\t\tB - Desistir e sair com %d", totalGanho);
+				
+				do {
+					
+					opc = toupper(getch()); //Função que converte uma letra miniscula em letra maiuscula
+				
+				} while(opc !="A" && opc != 'B');
+			}
+			
+			if(opc='B'){
+				
+				system("cls");
+				printf("\n\t\t\tOBbrigado por jogar AO QUEM QUER SER MILIONÁRIO? %s", nome);
+				
+				//TODO 
+				//Enviar nome e totalGanho para Ficheiro com Scores
+				
+				exit(1);
+				break;
+				
+			} else {
+				
+				system("cls");
+				
+				if(questaoNumero!=1){
+					printf("\n\t\t\tOK %s ! Vamos então avançar para a próxima questão!\n", nome);
+				}
+				
+				//-------------- QUESTÕES --------------------
+				printf("\t\t\tQuestão %d\n", questaoNumero);
+				printf("%s", perguntas[recebeQuestao].pergunta);
+				printf("%s", perguntas[recebeQuestao].opcA);
+				printf("%s", perguntas[recebeQuestao].opcB);
+				printf("%s", perguntas[recebeQuestao].opcC);
+				printf("%s", perguntas[recebeQuestao].opcD);
+			}
+		}
+	}
+	
+	void erroFicheiro(){
+		system("cls");
+		printf("\n\n\t\tErro ao Abrir o Ficheiro!");
+		exit(1);
+	}
     
     
 
